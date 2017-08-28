2a8e0b31792050c4668f7865cb79852886168e88
m41k <maik.alberto@hotmail.com>
2017-08-28 01:18:19 +0000

    Add files via upload

--- CliVGate/clivgate.sh
+++ CliVGate/clivgate.sh
@@ -1,21 +1,30 @@
 #!/bin/bash
-
+#Client for VPNGate
+#Created by:Maik Alberto - github.com/m41k
 main (){
 
 ifconfig | grep tun
  if [ $? -eq 0 ]; then
   IMG='changes-prevent'
   TXT='CONECTADO'
+#  BTN='Desonectar'
+#  ACT='1'
+  BTN='Desconetcar:1'
  else
   IMG='changes-allow'
   TXT='DESCONECTADO'
+#  BTN='Conectar'
+#  ACT='0'
+  BTN='Conetcar:0'
  fi
 
 yad --form --image=$IMG --image-on-top --title='CliVGATE' \
     --text-align='center' --text="<big><b>$TXT</b></big>"\
-    --button='Conectar':0 \
-    --button='Desconetcar':1 \
-    --window-icon="network-vpn-symbolic"
+    --window-icon="network-vpn-symbolic" \
+    --button=$BTN
+#    --button="$BTN":$ACT
+#    --button='Conectar':0 \
+#    --button='Desconetcar':1 \
 #    --text="<i>By Maik Alberto</i>"\
 #    --image=network-wired-symbolic
 
@@ -27,10 +36,14 @@ case $? in
 	 tail -n +3 index.html | cut -d "," -f15 | base64 -d > teste64 2> /dev/null
 	 openvpn --config teste64 &
          sleep 10
-	 main
+ 	 yad --notification  \
+         --image=network-vpn \
+	 --text "CliVGATE" \
+	 --command=./$0
+#	 main
 	;;
 	1)
-	killall openvpn
+	killall openvpn &
 esac
 }
 main
